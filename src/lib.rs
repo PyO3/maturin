a9642b64cc6e40dab9eae9e61ddfbfb06e81ab52
konstin <konstin@mailbox.org>
2018-08-21 12:27:05 +0200

    Remove unused extern crates

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,6 @@ extern crate regex;
 extern crate failure;
 extern crate atty;
 extern crate reqwest;
-extern crate rpassword;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;

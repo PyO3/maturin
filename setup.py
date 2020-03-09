d5bbab8dfbbad5c30f9f1313501b906a180e005c
konstin <konstin@mailbox.org>
2020-03-09 13:24:16 +0100

    Prepare for publishing with drone ci

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     "python packages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.7.9",
+    version="0.8.0-alpha.1",
     license="MIT OR Apache-2.0",
     python_requires=">=3.5",
     cmdclass={"install": PostInstallCommand, "bdist_wheel": bdist_wheel},

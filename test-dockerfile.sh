1f86826bc472aa02a70813b5058976e4eec449e8
konstin <konstin@mailbox.org>
2020-10-08 20:44:57 +0200

    Fixup

--- test-dockerfile.sh
+++ test-dockerfile.sh
@@ -14,7 +14,7 @@ docker run --rm -v $(pwd)/test-crates/hello-world:/io maturin build --no-sdist -
 
 venv-docker/bin/pip install hello-world --no-index --find-links test-crates/hello-world/target/wheels/
 
-if [[ $(python test-crates/hello-world/check_installed/check_installed.py) != 'SUCCESS' ]]; then
+if [[ $(venv-docker/bin/python test-crates/hello-world/check_installed/check_installed.py) != 'SUCCESS' ]]; then
   exit 1
 fi
 
@@ -22,7 +22,7 @@ docker run --rm -v $(pwd)/test-crates/cffi-pure:/io maturin build --no-sdist -b 
 
 venv-docker/bin/pip install cffi-pure --no-index --find-links test-crates/cffi-pure/target/wheels/
 
-if [[ $(python test-crates/cffi-pure/check_installed/check_installed.py) != 'SUCCESS' ]]; then
+if [[ $(venv-docker/bin/python test-crates/cffi-pure/check_installed/check_installed.py) != 'SUCCESS' ]]; then
   exit 1
 fi
 
@@ -30,7 +30,7 @@ docker run --rm -v $(pwd)/test-crates/cffi-mixed:/io maturin build --no-sdist -b
 
 venv-docker/bin/pip install cffi-mixed --no-index --find-links test-crates/cffi-mixed/target/wheels/
 
-if [[ $(python test-crates/cffi-mixed/check_installed/check_installed.py) != 'SUCCESS' ]]; then
+if [[ $(venv-docker/bin/python test-crates/cffi-mixed/check_installed/check_installed.py) != 'SUCCESS' ]]; then
   exit 1
 fi
 
@@ -38,7 +38,7 @@ docker run --rm -v $(pwd)/test-crates/pyo3-pure:/io maturin build --no-sdist -i 
 
 venv-docker/bin/pip install pyo3-pure --no-index --find-links test-crates/pyo3-pure/target/wheels/
 
-if [[ $(python test-crates/pyo3-pure/check_installed/check_installed.py) != 'SUCCESS' ]]; then
+if [[ $(venv-docker/bin/python test-crates/pyo3-pure/check_installed/check_installed.py) != 'SUCCESS' ]]; then
   exit 1
 fi
 
@@ -46,7 +46,7 @@ docker run --rm -v $(pwd)/test-crates/pyo3-mixed:/io maturin build --no-sdist -i
 
 venv-docker/bin/pip install pyo3-mixed --no-index --find-links test-crates/pyo3-mixed/target/wheels/
 
-if [[ $(python test-crates/pyo3-mixed/check_installed/check_installed.py) != 'SUCCESS' ]]; then
+if [[ $(venv-docker/bin/python test-crates/pyo3-mixed/check_installed/check_installed.py) != 'SUCCESS' ]]; then
   exit 1
 fi
 

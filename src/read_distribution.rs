7c2ff96158e0b3a91b97d10ba10c4ba0cb40c803
konstin <konstin@mailbox.org>
2020-12-01 23:28:06 +0100

    Read PKG-INFO for source distribution metadata

--- src/read_distribution.rs
+++ src/read_distribution.rs
@@ -1,8 +1,9 @@
 use anyhow::{bail, Context, Result};
+use flate2::read::GzDecoder;
 use fs_err::File;
 use mailparse::parse_mail;
 use std::io::{BufReader, Read};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use zip::ZipArchive;
 
 fn filename_from_file(path: impl AsRef<Path>) -> Result<String> {
@@ -13,6 +14,24 @@ fn filename_from_file(path: impl AsRef<Path>) -> Result<String> {
         .to_str()
         .context("Expected a utf-8 filename")?
         .to_string())
+}
+
+/// Read the email format into key value pairs
+fn metadata_from_bytes(metadata_email: &mut Vec<u8>) -> Result<Vec<(String, String)>> {
+    let metadata_email = parse_mail(&metadata_email).context("Failed to parse METADATA")?;
+
+    let mut metadata = Vec::new();
+    for header in &metadata_email.headers {
+        metadata.push((header.get_key().to_string(), header.get_value().to_string()));
+    }
+
+    let body = metadata_email
+        .get_body()
+        .context("Failed to parse METADATA")?;
+    if !body.trim().is_empty() {
+        metadata.push(("Description".into(), body));
+    }
+    Ok(metadata)
 }
 
 /// Reads the METADATA file in the .dist-info directory of a wheel, returning
@@ -43,40 +62,37 @@ fn read_metadata_for_wheel(path: impl AsRef<Path>) -> Result<Vec<(String, String
         .read_to_end(&mut metadata_email)
         .context("Failed to read METADATA file")?;
 
-    let metadata_email = parse_mail(&metadata_email).context("Failed to parse METADATA")?;
-
-    let mut metadata = Vec::new();
-    for header in &metadata_email.headers {
-        metadata.push((header.get_key().to_string(), header.get_value().to_string()));
-    }
-
-    let body = metadata_email
-        .get_body()
-        .context("Failed to parse METADATA")?;
-    if !body.trim().is_empty() {
-        metadata.push(("Description".into(), body));
-    }
-    Ok(metadata)
+    metadata_from_bytes(&mut metadata_email)
 }
 
 /// Returns the metadata for a source distribution (.tar.gz).
 /// Only parses the filename since dist-info is not part of source
 /// distributions
 fn read_metadata_for_source_distribution(path: impl AsRef<Path>) -> Result<Vec<(String, String)>> {
-    let filename = filename_from_file(&path)?
-        .strip_suffix(".tar.gz")
-        // We checked that before entering this function
-        .unwrap()
-        .to_string();
-    let metadata = match filename.split('-').collect::<Vec<_>>().as_slice() {
-        [name, version] => vec![
-            ("Metadata-Version".to_string(), "2.1".to_string()),
-            ("Name".to_string(), name.to_string()),
-            ("Version".to_string(), version.to_string()),
-        ],
-        _ => bail!("Invalid wheel name: {}", filename),
-    };
-    Ok(metadata)
+    let mut reader = tar::Archive::new(GzDecoder::new(BufReader::new(File::open(path.as_ref())?)));
+    // Unlike for wheels, in source distributions the metadata is stored in a file called PKG-INFO
+    // try_find would be ideal here, but it's nightly only
+    let mut entry = reader
+        .entries()?
+        .map(|entry| -> Result<_> {
+            let entry = entry?;
+            if entry.path()? == PathBuf::from("PKG-INFO") {
+                Ok(Some(entry))
+            } else {
+                Ok(None)
+            }
+        })
+        .find_map(|x| x.transpose())
+        .context(format!(
+            "Source distribution {:?} does not contain a PKG-INFO, but it should",
+            path.as_ref()
+        ))?
+        .context(format!("Failed to read {:?}", path.as_ref()))?;
+    let mut metadata_email = Vec::new();
+    entry
+        .read_to_end(&mut metadata_email)
+        .context(format!("Failed to read {:?}", path.as_ref()))?;
+    metadata_from_bytes(&mut metadata_email)
 }
 
 /// Returns the metadata as key value pairs for a wheel or a source distribution
@@ -103,14 +119,18 @@ mod test {
     fn test_source_distribution() {
         let metadata =
             get_metadata_for_distribution(Path::new("test-data/pyo3_mixed-2.1.1.tar.gz")).unwrap();
-        assert_eq!(
-            metadata,
-            vec![
-                ("Metadata-Version".to_string(), "2.1".to_string()),
-                ("Name".to_string(), "pyo3_mixed".to_string()),
-                ("Version".to_string(), "2.1.1".to_string())
-            ]
-        );
+        let expected: Vec<_> = [
+            ("Metadata-Version", "2.1"),
+            ("Name", "pyo3-mixed"),
+            ("Version", "2.1.1"),
+            ("Summary", "Implements a dummy function combining rust and python"),
+            ("Author", "konstin <konstin@mailbox.org>"),
+            ("Author-Email", "konstin <konstin@mailbox.org>"),
+            ("Description-Content-Type", "text/markdown; charset=UTF-8; variant=GFM"),
+            ("Description", "# pyo3-mixed\n\nA package for testing maturin with a mixed pyo3/python project.\n\n"),
+        ].iter().map(|(k,v)| (k.to_string(), v.to_string())).collect();
+
+        assert_eq!(metadata, expected);
     }
 
     #[test]

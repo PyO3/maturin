d2bf87b1ff5f2ee1b599f6d8fa7f046b10f65ca5
konstin <konstin@mailbox.org>
2021-04-24 19:06:20 +0200

    Forbid caret in requires-python (#511)
    pip 21.1 complained it doesn't match PEP 440

--- tests/common/integration.rs
+++ tests/common/integration.rs
@@ -2,7 +2,7 @@ use crate::common::{adjust_canonicalization, check_installed, maybe_mock_cargo};
 use anyhow::{bail, Context, Result};
 use maturin::{BuildOptions, Target};
 use std::path::{Path, PathBuf};
-use std::process::{Command, Stdio};
+use std::process::Command;
 use std::str;
 use structopt::StructOpt;
 
@@ -85,22 +85,31 @@ pub fn test_integration(package: impl AsRef<Path>, bindings: Option<String>) -> 
         let command = [
             "-m",
             "pip",
+            "--disable-pip-version-check",
             "install",
             "--force-reinstall",
             &adjust_canonicalization(filename),
         ];
         let output = Command::new(&python)
             .args(&command)
-            .stderr(Stdio::inherit())
             .output()
             .context(format!("pip install failed with {:?}", python))?;
         if !output.status.success() {
             bail!(
-                "pip install failed running {:?}: {}\n--- Stdout:\n{}\n--- Stderr:\n{}",
+                "pip install failed running {:?}: {}\n--- Stdout:\n{}\n--- Stderr:\n{}\n---\n",
                 &command,
                 output.status,
-                str::from_utf8(&output.stdout)?,
-                str::from_utf8(&output.stderr)?,
+                str::from_utf8(&output.stdout)?.trim(),
+                str::from_utf8(&output.stderr)?.trim(),
+            );
+        }
+        if !output.stderr.is_empty() {
+            bail!(
+                "pip raised a warning running {:?}: {}\n--- Stdout:\n{}\n--- Stderr:\n{}\n---\n",
+                &command,
+                output.status,
+                str::from_utf8(&output.stdout)?.trim(),
+                str::from_utf8(&output.stderr)?.trim(),
             );
         }
 
@@ -127,6 +136,7 @@ fn create_conda_env(name: &str, major: usize, minor: usize) {
 #[cfg(target_os = "windows")]
 pub fn test_integration_conda(package: impl AsRef<Path>, bindings: Option<String>) -> Result<()> {
     use std::env;
+    use std::process::Stdio;
 
     let package_string = package.as_ref().join("Cargo.toml").display().to_string();
 
@@ -184,6 +194,7 @@ pub fn test_integration_conda(package: impl AsRef<Path>, bindings: Option<String
             .args(&[
                 "-m",
                 "pip",
+                "--disable-pip-version-check",
                 "install",
                 "--force-reinstall",
                 &adjust_canonicalization(wheel_file),

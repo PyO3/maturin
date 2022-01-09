5ba53c6995cba35942b44d935c5ea535b2b92ccc
messense <messense@icloud.com>
2022-01-09 15:11:21 +0800

    Allow running tests without zig

--- tests/common/integration.rs
+++ tests/common/integration.rs
@@ -1,7 +1,7 @@
 use crate::common::{adjust_canonicalization, check_installed, maybe_mock_cargo};
 use anyhow::{bail, Context, Result};
 use clap::Parser;
-use maturin::{BuildOptions, PythonInterpreter, Target};
+use maturin::{BuildOptions, PythonInterpreter, Target, Zig};
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str;
@@ -43,7 +43,7 @@ pub fn test_integration(
         cli.push(bindings);
     }
 
-    if zig {
+    if zig && Zig::find_zig().is_ok() {
         cli.push("--zig")
     } else {
         cli.push("--compatibility");

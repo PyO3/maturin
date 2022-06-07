c51fc628dda88ae15e71db19f00d991ca95d0aef
messense <messense@icloud.com>
2022-06-07 15:15:50 +0800

    Don't graft `/lib/ld-musl-*.so.1` on musllinux

--- src/auditwheel/repair.rs
+++ src/auditwheel/repair.rs
@@ -14,14 +14,16 @@ pub fn find_external_libs(
         .analyze(artifact)
         .map_err(AuditWheelError::DependencyAnalysisError)?;
     let mut ext_libs = Vec::new();
-    for (name, lib) in deps.libraries {
+    for (_, lib) in deps.libraries {
+        let name = &lib.name;
         // Skip dynamic linker/loader and white-listed libs
         if name.starts_with("ld-linux")
             || name == "ld64.so.2"
             || name == "ld64.so.1"
             // musl libc, eg: libc.musl-aarch64.so.1
+            || name.starts_with("ld-musl")
             || name.starts_with("libc.")
-            || policy.lib_whitelist.contains(&name)
+            || policy.lib_whitelist.contains(name)
         {
             continue;
         }

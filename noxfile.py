437b5fe2e828d9d599db2471ae1845ee4d6f044a
Hood Chatham <roberthoodchatham@gmail.com>
2022-06-24 13:10:48 -0700

    Improvements to Emscripten/Pyodide CI

--- noxfile.py
+++ noxfile.py
@@ -1,33 +1,14 @@
 import sys
 from pathlib import Path
+import os
 
 import nox
-
-
-def download_pyodide(session: nox.Session, pyodide_dir: Path) -> None:
-    pyodide_dir.mkdir()
-
-    PYODIDE_DEV = "https://pyodide-cdn2.iodide.io/dev/full/"
-    pyodide_files = [
-        "pyodide.js",
-        "repodata.json",
-        "pyodide.asm.js",
-        "pyodide.asm.data",
-        "pyodide.asm.wasm",
-        "pyodide_py.tar",
-    ]
-    with session.chdir(pyodide_dir):
-        for file in pyodide_files:
-            session.run("wget", "-q", PYODIDE_DEV + file, external=True)
-        session.run("npm", "i", "node-fetch", external=True)
-
 
 @nox.session(name="test-emscripten")
 def test_emscripten(session: nox.Session):
     emscripten_dir = Path("./tests").resolve()
-    pyodide_dir = emscripten_dir / "pyodide"
-    if not pyodide_dir.exists():
-        download_pyodide(session, pyodide_dir)
+    with session.chdir(emscripten_dir):
+        session.run("npm", "i", "pyodide@" + os.environ["PYODIDE_VERSION"], external=True)
 
     test_crates = [
         "test-crates/pyo3-pure",
--- tests/emscripten_runner.js
+++ tests/emscripten_runner.js
@@ -1,36 +1,5 @@
-const http = require("http");
-const fs = require("fs");
 const { opendir } = require("node:fs/promises");
-
-const { loadPyodide } = require("./pyodide/pyodide.js");
-
-const PORT = 8124;
-
-const server = http
-  .createServer(function (request, response) {
-    const filePath = "." + request.url;
-    const contentType = "application/octet-stream";
-    fs.readFile(
-      process.argv[2] + "/target/wheels/" + filePath,
-      function (error, content) {
-        if (error) {
-          if (error.code == "ENOENT") {
-            response.writeHead(404);
-            response.end("Not found");
-            response.end();
-          } else {
-            response.writeHead(500);
-            response.end("error: " + error.code);
-            response.end();
-          }
-        } else {
-          response.writeHead(200, { "Content-Type": contentType });
-          response.end(content, "utf-8");
-        }
-      }
-    );
-  })
-  .listen(PORT);
+const { loadPyodide } = require("pyodide");
 
 async function findWheel(distDir) {
   const dir = await opendir(distDir);
@@ -41,35 +10,29 @@ async function findWheel(distDir) {
   }
 }
 
-const localhost = `http://0.0.0.0:${PORT}`;
 const pkgDir = process.argv[2];
 const distDir = pkgDir + "/target/wheels";
 const testDir = pkgDir + "/tests";
 
 async function main() {
   const wheelName = await findWheel(distDir);
-  const wheelURL = `${localhost}/${wheelName}`;
+  const wheelURL = `file:${distDir}/${wheelName}`;
 
-  let errcode = 1;
   try {
-    pyodide = await loadPyodide({ indexURL: "./pyodide", fullStdLib: false });
-    pyodide._api.setCdnUrl("https://pyodide-cdn2.iodide.io/dev/full/");
+    pyodide = await loadPyodide();
     const FS = pyodide.FS;
     const NODEFS = FS.filesystems.NODEFS;
     FS.mkdir("/test_dir");
     FS.mount(NODEFS, { root: testDir }, "/test_dir");
-    await pyodide.loadPackage(["micropip", "pytest", "tomli"]);
+    await pyodide.loadPackage(["packaging", "file:///home/hood/Documents/programming/pyodide/dist/micropip-0.1-py3-none-any.whl", "pytest", "tomli"]);
     const micropip = pyodide.pyimport("micropip");
     await micropip.install(wheelURL);
     const pytest = pyodide.pyimport("pytest");
     errcode = pytest.main(pyodide.toPy(["/test_dir", "-vv"]));
   } catch (e) {
     console.error(e);
-    errcode = 1;
-  } finally {
-    server.close();
+    process.exit(1);
   }
-  process.exit(errcode);
 }
 
 main();

04fe216675077a3ab6972120d186e77326764d8b
messense <messense@icloud.com>
2022-09-01 19:18:31 +0800

    Upgrade pyodide to 0.21.2

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import nox
 
 
-PYODIDE_VERSION = os.getenv("PYODIDE_VERSION", "0.21.1")
+PYODIDE_VERSION = os.getenv("PYODIDE_VERSION", "0.21.2")
 GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS")
 GITHUB_ENV = os.getenv("GITHUB_ENV")
 

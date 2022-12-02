04555e7bd685cdaaf9674d9722f2570e02e61b4a
messense <messense@icloud.com>
2022-12-02 14:00:44 +0800

    Use Python 3.11 in docker test

--- test-dockerfile.sh
+++ test-dockerfile.sh
@@ -5,7 +5,7 @@
 set -e
 
 rm -rf venv-docker
-python3.8 -m venv venv-docker
+python3.11 -m venv venv-docker
 venv-docker/bin/pip install -U pip cffi
 
 # FIXME: Can we run the tests without activate? Currently hello-world fails because then the binary is not in PATH
@@ -14,7 +14,7 @@ source venv-docker/bin/activate
 for test_crate in hello-world cffi-pure cffi-mixed pyo3-pure pyo3-mixed pyo3-mixed-submodule
 do
   echo "Testing $test_crate"
-  docker run -e RUST_BACKTRACE=1 --rm -v "$(pwd):/io" -w /io/test-crates/$test_crate maturin build -i python3.8
+  docker run -e RUST_BACKTRACE=1 --rm -v "$(pwd):/io" -w /io/test-crates/$test_crate maturin build -i python3.11
   # --only-binary=:all: stops pip from picking a local already compiled sdist
   venv-docker/bin/pip install $test_crate --only-binary=:all: --find-links test-crates/$test_crate/target/wheels/
   if [[ $(venv-docker/bin/python test-crates/$test_crate/check_installed/check_installed.py) != 'SUCCESS' ]]; then

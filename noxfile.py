c2aff4e3c5cbb38949a4ff2808df93048b936ba1
messense <messense@icloud.com>
2023-04-01 12:36:27 +0800

    Update pyodide to 0.23.0

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import nox
 
 
-PYODIDE_VERSION = os.getenv("PYODIDE_VERSION", "0.22.0")
+PYODIDE_VERSION = os.getenv("PYODIDE_VERSION", "0.23.0")
 GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS")
 GITHUB_ENV = os.getenv("GITHUB_ENV")
 

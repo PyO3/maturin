cfcc8e2d1cd3d4d4f2dea1c9c0793a657d272df6
konstin <konstin@mailbox.org>
2023-04-11 19:25:36 +0200

    Document binary and library in a single pacakge by entrypoint workaround

--- test-crates/pyo3-mixed/check_installed/check_installed.py
+++ test-crates/pyo3-mixed/check_installed/check_installed.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python3
+import json
+import platform
+import sys
+from pathlib import Path
+from subprocess import check_output
 
 from boltons.strutils import slugify
+
 import pyo3_mixed
 
 assert pyo3_mixed.get_42() == 42
 assert slugify("First post! Hi!!!!~1    ") == "first_post_hi_1"
+
+script_name = "print_cli_args"
+args = ["a", "b", "c"]
+[rust_args, python_args] = check_output([script_name, *args], text=True).splitlines()
+# The rust vec debug format is also valid json
+rust_args = json.loads(rust_args)
+python_args = json.loads(python_args)
+
+# On linux we get sys.executable, windows resolve the path and mac os gives us a third
+# path (
+# {prefix}/Python.framework/Versions/3.10/Resources/Python.app/Contents/MacOS/Python
+# vs
+# {prefix}/Python.framework/Versions/3.10/bin/python3.10
+# on cirrus ci)
+# We also skip windows because cpython resolves while pypy doesn't
+if platform.system() == "Linux":
+    assert rust_args[0] == sys.executable, (rust_args, sys.executable)
+
+# Windows can't decide if it's with or without .exe, FreeBSB just doesn't work for some reason
+if platform.system() in ["Darwin", "Linux"]:
+    # Unix venv layout (and hopefully also on more exotic platforms)
+    print_cli_args = str(Path(sys.prefix).joinpath("bin").joinpath(script_name))
+    assert rust_args[1] == print_cli_args, (rust_args, print_cli_args)
+    assert python_args[0] == print_cli_args, (python_args, print_cli_args)
+
+# FreeBSB just doesn't work for some reason
+if platform.system() in ["Darwin", "Linux", "Windows"]:
+    # Rust contains the python executable as first argument but python does not
+    assert rust_args[2:] == args, rust_args
+    assert python_args[1:] == args, python_args
 
 print("SUCCESS")

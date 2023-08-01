a438fcf5b6ef85543f8dceda85ce133c14e1ea7c
Patterson, Kevin R <kevin.r.patterson@intel.com>
2023-08-01 13:02:06 -0500

    black fixes

--- test-crates/pyo3-mixed-implicit/tests/test_pyo3_mixed_implicit.py
+++ test-crates/pyo3-mixed-implicit/tests/test_pyo3_mixed_implicit.py
@@ -2,9 +2,10 @@
 
 import pyo3_mixed_implicit.some_rust as some_rust
 
+
 def test_get_rust_and_python():
     assert some_rust.get_22() == 22
     assert some_rust.double(lambda: 4) == 8
 
-print("SUCCESS")
 
+print("SUCCESS")

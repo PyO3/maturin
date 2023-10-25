9e16a59f0a6353f0642fa37be7345206ec90572c
messense <messense@icloud.com>
2023-10-25 19:34:51 +0800

    Switch to Ruff formatter

--- noxfile.py
+++ noxfile.py
@@ -40,9 +40,7 @@ def setup_pyodide(session: nox.Session):
                 external=True,
             )
             with open("repodata.json") as f:
-                emscripten_version = (
-                    json.load(f)["info"]["platform"].split("_", 1)[1].replace("_", ".")
-                )
+                emscripten_version = json.load(f)["info"]["platform"].split("_", 1)[1].replace("_", ".")
                 append_to_github_env("EMSCRIPTEN_VERSION", emscripten_version)
 
 

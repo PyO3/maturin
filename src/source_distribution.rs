c7ac62d11a0d01bcf3eebd7de58ad9c128df54c8
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2023-10-31 09:26:30 -0600

    strip workspace root instead of using filename

--- src/source_distribution.rs
+++ src/source_distribution.rs
@@ -456,7 +456,7 @@ fn add_cargo_package_files_to_sdist(
             let mut deps_to_keep = known_path_deps.clone();
             // Also need to the main Python binding crate
             let main_member_name = abs_manifest_dir
-                .file_name()
+                .strip_prefix(&workspace_root)
                 .unwrap()
                 .to_str()
                 .unwrap()

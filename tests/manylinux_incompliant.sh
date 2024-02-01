75aba5ca0c4f9d6094e636cdd0a343da89befec4
messense <messense@icloud.com>
2024-02-01 02:31:29 +0000

    Use manylinux_2_28 in auditwheel test

--- tests/manylinux_incompliant.sh
+++ tests/manylinux_incompliant.sh
@@ -3,7 +3,7 @@ set -e
 
 which cargo > /dev/null || curl -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal
 
-# Fail because we're running in manylinux2014, which can't build for manylinux 2010
+# Fail because we're running in manylinux2_28, which can't build for manylinux 2010
 for PYBIN in /opt/python/cp3[9]*/bin; do
   if cargo run -- build -m test-crates/pyo3-mixed/Cargo.toml --target-dir test-crates/targets -i "${PYBIN}/python" --manylinux 2010 -o dist; then
     echo "maturin build unexpectedly succeeded"

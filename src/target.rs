fa64426f3a98a0455721c23ec86bd2240708b45e
Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
2024-09-01 15:55:07 +0300

    Extract extension ABI name resolvation code as helper
    This commit introduces helper InterpreterConfig.get_python_target_env()
    that can be used to determine the extension ABI python uses in
    `ext_suffix` for this architecture.
    
    Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

--- src/target.rs
+++ src/target.rs
@@ -1,5 +1,6 @@
 use crate::cross_compile::is_cross_compiling;
 use crate::python_interpreter::InterpreterKind;
+use crate::python_interpreter::InterpreterKind::{CPython, GraalPy, PyPy};
 use crate::PlatformTag;
 use anyhow::{anyhow, bail, format_err, Result};
 use platform_info::*;
@@ -381,6 +382,25 @@ impl Target {
             "x86"
         } else {
             self.get_python_arch()
+        }
+    }
+
+    /// Returns the environment python uses in `ext_suffix` for this architecture.
+    pub fn get_python_target_env(
+        &self,
+        python_impl: InterpreterKind,
+        python_version: (usize, usize),
+    ) -> String {
+        match python_impl {
+            CPython => {
+                // For musl handling see https://github.com/pypa/auditwheel/issues/349
+                if python_version >= (3, 11) {
+                    self.target_env().to_string()
+                } else {
+                    self.target_env().to_string().replace("musl", "gnu")
+                }
+            }
+            PyPy | GraalPy => "gnu".to_string(),
         }
     }
 

6090c81ff5103e260055c5d20e3f14212e7c4257
konsti <konstin@mailbox.org>
2024-11-28 16:11:36 +0100

    Bump metadata version to 2.4 to fix license bug (#2332)
    When a project sets `license = { file = "LICENSE" }`, we're currently
    emitting a `License-File` entry, but Metadata-Version 2.3, which is
    invalid and now gets rejected by PyPI, breaking publish for projects
    using `license = { file = "LICENSE" }`. This change bumps the metadata
    version.
    
    Alternatively, we could only bump the version if license files are used
    and use the old `License` field for `license = { file = "LICENSE" }`
    again, but i'm not aware of any index currently rejecting metadata
    version 2.4.

--- src/metadata.rs
+++ src/metadata.rs
@@ -70,7 +70,7 @@ impl Metadata23 {
     /// Initializes with name, version and otherwise the defaults
     pub fn new(name: String, version: Version) -> Self {
         Self {
-            metadata_version: "2.3".to_string(),
+            metadata_version: "2.4".to_string(),
             name,
             version,
             platform: vec![],
@@ -679,7 +679,7 @@ mod test {
         );
 
         let expected = expect![[r#"
-            Metadata-Version: 2.3
+            Metadata-Version: 2.4
             Name: info-project
             Version: 0.1.0
             Summary: A test project

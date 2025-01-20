e81d71cfb488a93c1083d08e0793ea4795123ee0
TheZoq2 <frans.skarman@protonmail.com>
2025-01-20 16:04:17 +0100

    Fix constant rebuilds

--- src/compile.rs
+++ src/compile.rs
@@ -441,12 +441,23 @@ fn cargo_build_command(
                 target_triple, interpreter.major, interpreter.minor
             ));
             fs::create_dir_all(&maturin_target_dir)?;
-            fs::write(&config_file, pyo3_config).with_context(|| {
-                format!(
-                    "Failed to create pyo3 config file at '{}'",
-                    config_file.display()
-                )
-            })?;
+            // We don't want to rewrite the file every time as that will make cargo
+            // trigger a rebuild of the project every time
+            let rewrite = if config_file.exists() {
+                std::fs::read_to_string(&config_file)
+                    .map(|previous| previous != pyo3_config)
+                    .unwrap_or(false)
+            } else {
+                true
+            };
+            if rewrite {
+                fs::write(&config_file, pyo3_config).with_context(|| {
+                    format!(
+                        "Failed to create pyo3 config file at '{}'",
+                        config_file.display()
+                    )
+                })?;
+            }
             let abs_config_file = config_file.normalize()?.into_path_buf();
             build_command.env("PYO3_CONFIG_FILE", abs_config_file);
         }

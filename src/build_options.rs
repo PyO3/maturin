ae7a34c4e1464ce6ea1d9984cc51171300c1fc32
Russell Keith-Magee <russell@keith-magee.com>
2025-11-07 15:18:57 +0800

    Correct formatting problems.

--- src/build_options.rs
+++ src/build_options.rs
@@ -6,7 +6,9 @@ use crate::cross_compile::{find_sysconfigdata, parse_sysconfigdata};
 use crate::project_layout::ProjectResolver;
 use crate::pyproject_toml::ToolMaturin;
 use crate::python_interpreter::{InterpreterConfig, InterpreterKind};
-use crate::target::{detect_arch_from_python, detect_target_from_cross_python, is_arch_supported_by_pypi};
+use crate::target::{
+    detect_arch_from_python, detect_target_from_cross_python, is_arch_supported_by_pypi,
+};
 use crate::{BridgeModel, BuildContext, PyO3, PythonInterpreter, Target};
 use anyhow::{bail, format_err, Context, Result};
 use cargo_metadata::{CrateType, PackageId, TargetKind};
@@ -668,13 +670,13 @@ impl BuildContextBuilder {
                 if let Some(detected_target) = detect_arch_from_python(interpreter, &target) {
                     target = Target::from_target_triple(Some(&detected_target))?;
                 }
-            }
-            else {
+            } else {
                 // If there's no explicit user-provided target or interpreter,
                 // check the interpreter; if the interpreter identifies as a
                 // cross compiler, set the target based on the platform reported
                 // by the interpreter.
-                if let Some(detected_target) = detect_target_from_cross_python(&target.get_python()) {
+                if let Some(detected_target) = detect_target_from_cross_python(&target.get_python())
+                {
                     target = Target::from_target_triple(Some(&detected_target))?;
                 }
             }

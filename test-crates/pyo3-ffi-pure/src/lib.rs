685efba876ad23417d506e40f21c8bebacb0c00f
David Hewitt <mail@davidhewitt.dev>
2025-11-08 04:25:03 +0000

    ci: bump to Python 3.14, update runners (#2830)

--- test-crates/pyo3-ffi-pure/src/lib.rs
+++ test-crates/pyo3-ffi-pure/src/lib.rs
@@ -1,42 +1,60 @@
+use std::ptr;
+
 use pyo3_ffi::*;
-use std::os::raw::c_char;
 
-#[allow(non_snake_case)]
-#[no_mangle]
-pub unsafe extern "C" fn PyInit_pyo3_ffi_pure() -> *mut PyObject {
-    let module_name = "pyo3_ffi_pure\0".as_ptr() as *const c_char;
-    let init = PyModuleDef {
-        m_base: PyModuleDef_HEAD_INIT,
-        m_name: module_name,
-        m_doc: std::ptr::null(),
-        m_size: 0,
-        m_methods: std::ptr::null_mut(),
-        m_slots: std::ptr::null_mut(),
-        m_traverse: None,
-        m_clear: None,
-        m_free: None,
-    };
-    let mptr = PyModule_Create(Box::into_raw(Box::new(init)));
+static mut MODULE_DEF: PyModuleDef = PyModuleDef {
+    m_base: PyModuleDef_HEAD_INIT,
+    m_name: c"string_sum".as_ptr(),
+    m_doc: c"A Python module written in Rust.".as_ptr(),
+    m_size: 0,
+    m_methods: std::ptr::addr_of_mut!(METHODS).cast(),
+    m_slots: std::ptr::addr_of_mut!(SLOTS).cast(),
+    m_traverse: None,
+    m_clear: None,
+    m_free: None,
+};
 
-    let wrapped_sum = PyMethodDef {
-        ml_name: "sum\0".as_ptr() as *const c_char,
+static mut METHODS: [PyMethodDef; 2] = [
+    PyMethodDef {
+        ml_name: c"sum".as_ptr(),
         ml_meth: PyMethodDefPointer {
             PyCFunctionWithKeywords: sum,
         },
         ml_flags: METH_VARARGS | METH_KEYWORDS,
-        ml_doc: std::ptr::null_mut(),
-    };
-    PyModule_AddObject(
-        mptr,
-        "sum\0".as_ptr() as *const c_char,
-        PyCFunction_NewEx(
-            Box::into_raw(Box::new(wrapped_sum)),
-            std::ptr::null_mut(),
-            PyUnicode_InternFromString(module_name),
-        ),
-    );
+        ml_doc: c"returns the sum of two integers".as_ptr(),
+    },
+    // A zeroed PyMethodDef to mark the end of the array.
+    PyMethodDef::zeroed(),
+];
 
-    mptr
+const SLOTS_LEN: usize =
+    1 + if cfg!(Py_3_12) { 1 } else { 0 } + if cfg!(Py_GIL_DISABLED) { 1 } else { 0 };
+
+static mut SLOTS: [PyModuleDef_Slot; SLOTS_LEN] = [
+    // NB: only include this slot if the module does not store any global state in `static` variables
+    // or other data which could cross between subinterpreters
+    #[cfg(Py_3_12)]
+    PyModuleDef_Slot {
+        slot: Py_mod_multiple_interpreters,
+        value: Py_MOD_PER_INTERPRETER_GIL_SUPPORTED,
+    },
+    // NB: only include this slot if the module does not depend on the GIL for thread safety
+    #[cfg(Py_GIL_DISABLED)]
+    PyModuleDef_Slot {
+        slot: Py_mod_gil,
+        value: Py_MOD_GIL_NOT_USED,
+    },
+    PyModuleDef_Slot {
+        slot: 0,
+        value: ptr::null_mut(),
+    },
+];
+
+// The module initialization function, which must be named `PyInit_<your_module>`.
+#[allow(non_snake_case)]
+#[no_mangle]
+pub unsafe extern "C" fn PyInit_pyo3_ffi_pure() -> *mut PyObject {
+    PyModuleDef_Init(ptr::addr_of_mut!(MODULE_DEF))
 }
 
 #[no_mangle]
